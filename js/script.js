20b63ce1fd79cf2219ed25b1155ca354da46f910
Sergey Koternyak <sergeykoternyak@gmail.com>
2016-07-23 19:47:26 +0300

    Added and update files

--- js/script.js
+++ js/script.js
@@ -4,6 +4,21 @@ $(function(){
 		$('.navigation-list').toggle()
 	});
 
+	// Top slider
+	function getRandomNumber(min, max){
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	setInterval(function(){
+		var el = $('.flipper')[getRandomNumber(0, 6)];
+
+		if($(el).hasClass('is-show')){
+			$(el).find('.front').css('background-image', 'url(styles/imgs/top-slide-' + getRandomNumber(0, 9) + '.png)');
+		} else{
+			$($(el).find('.back')).css('background-image', 'url(styles/imgs/top-slide-' + getRandomNumber(0, 9) + '.png)');
+		}
+		$(el).hasClass('is-show') ? $(el).removeClass('is-show') : $(el).addClass('is-show');
+	}, 3000)
+
 	// Partners list
 	$(".tab-list-mode, .tab").click(function(){
 		if(!$(this).hasClass('active')) {
