c89c6e09a84defb6636449f9022f521930fc159a
Sergey Koternyak <sergeykoternyak@gmail.com>
2016-07-24 13:34:40 +0300

    Added and update files

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(function(){
 			$($(el).find('.back')).css('background-image', 'url(styles/imgs/top-slide-' + getRandomNumber(0, 9) + '.png)');
 		}
 		$(el).hasClass('is-show') ? $(el).removeClass('is-show') : $(el).addClass('is-show');
-	}, 3000)
+	}, 3000);
 
 	// Partners list
 	$(".tab-list-mode, .tab").click(function(){
@@ -28,6 +28,47 @@ $(function(){
 			$(this).addClass("active");
 			$($(this).hasClass('tab') ? '.tab-list-mode' : '.tab').removeClass("active");
 			$(($(this).hasClass('tab') ? '.tab-list-mode' : '.tab') + "[data-name^='" + $(this).attr("data-name") + "']").addClass("active");
+		} else{
+			$(".tab-content").hide();
+			$(".tab-list-mode, .tab").removeClass("active");
 		}
-	})
+	});
+
+	// Round sliders
+	function roundSlider(name) {
+		var slideWidth = $(name+' .slide').width();
+		var slideSum = $(name+' .slide').length;
+		var $slideWrapper = $(name+' .slides-wrapper');
+
+	// CSS slide-wrapper width
+		$slideWrapper.width(slideSum*slideWidth);
+
+	// Обработчики кнопок вперед/назад
+		$(name+' .next').click(function(){
+			if ($slideWrapper.is(':animated')==false){
+				if (window.matchMedia("(max-width: 742px)").matches) {
+					$slideWrapper.animate({'margin-left': -slideWidth}, function(){
+						$slideWrapper.find('.slide:first').appendTo($slideWrapper).parent().css({'margin-left': 0});
+					});
+				} else{
+					$slideWrapper.animate({'margin-top': -slideWidth}, function(){
+						$slideWrapper.find('.slide:first').appendTo($slideWrapper).parent().css({'margin-top': 0});
+					});
+				}
+			};
+		});
+
+		$(name+' .prev').click(function(){
+			if ($slideWrapper.is(':animated')==false){
+				if (window.matchMedia("(max-width: 742px)").matches) {
+					$slideWrapper.find('.slide:last').prependTo($slideWrapper).parent().css({'margin-left': -slideWidth}).animate({'margin-left': 0});
+				} else{
+					$slideWrapper.find('.slide:last').prependTo($slideWrapper).parent().css({'margin-top': -slideWidth}).animate({'margin-top': 0});
+				}
+			}
+		});
+	}
+
+	roundSlider('.first');
+	roundSlider('.second');
 })
